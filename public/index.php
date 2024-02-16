d469c78bbdca20a1954b68c084a8a941c727f743
Bluffingo <chazpelo2@gmail.com>
2024-02-15 22:30:42 -0500

    start

---
+++ public/index.php
@@ -0,0 +1,20 @@
+<?php
+namespace Qobo;
+// Copyright Bluffingo 2024, all rights reserved.
+
+// todo: add a require_once to something that'll initalize stuff. that shit will go
+// in the private folder.
+
+// don't begin work on the frontend just yet. prototype the features first. 
+// this won't replace opensb for a while.
+?>
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Qobo</h1>
+<p>Qobo</p>
+
+</body>
+</html> 
