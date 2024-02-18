87765da73fe5a65719b2e6e4e21f850c3f3e199a
Bluffingo <chazpelo2@gmail.com>
2024-02-17 21:23:54 -0500

    okay just do shit

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# bitqobo
+# bitQobo
 Proprietary Qobo rewrite
+
+## Installation
+We'll assume you already have a webserver, preferably Apache. If this somehow gets leaked and you're reading this without being a contributor, we will not offer help regarding troubles setting it up.
+
+1. Run ``composer update`` in the terminal. This is required for dependencies to work.
+2. (Placeholder regarding DB user configuration, and likely BunnyCDN-related stuff)
+3. (Placeholder regarding DB importing, this should be done with a migration-esque system?)
+4. (Placeholder regarding compiling SCSS)
+
+### Apache virtual host
+Replace ``LOCATION`` with where your copy of bitQobo is located.
+
+```
+<VirtualHost *> 
+    ServerName localhost
+    DocumentRoot "LOCATION/site"
+
+    Alias /dynamic "LOCATION/dynamic"
+
+    <Directory "LOCATION">
+        Options Indexes FollowSymLinks
+        Require all granted
+        AllowOverride All
+    </Directory>
+</VirtualHost>
+```
---
+++ database/bitqobo.sql
@@ -0,0 +1,92 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 18, 2024 at 03:11 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `qobo_development`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `submissions`
+--
+
+CREATE TABLE `submissions` (
+  `id` int(11) NOT NULL,
+  `display_id` varchar(11) NOT NULL,
+  `title` varchar(128) NOT NULL,
+  `description` varchar(8192) NOT NULL,
+  `published` int(11) NOT NULL,
+  `author` int(11) NOT NULL,
+  `rating` enum('general','questionable','mature') NOT NULL,
+  `type` enum('video','image','music') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(128) NOT NULL,
+  `email` varchar(128) NOT NULL,
+  `passhash` varchar(128) NOT NULL,
+  `joined` int(11) NOT NULL,
+  `token` varchar(128) NOT NULL,
+  `rating` enum('general','questionable','mature') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `submissions`
+--
+ALTER TABLE `submissions`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `submissions`
+--
+ALTER TABLE `submissions`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
