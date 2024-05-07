7d08a9718c5bde8ade77ac4d7db497c2c44be24e
Bluffingo <chazpelo2@gmail.com>
2024-05-07 16:50:46 -0400

    fix compile-scss.sh so it works for everyone

--- tools/compile-scss.sh
+++ tools/compile-scss.sh
@@ -2,7 +2,12 @@
 
 # Copyright Chaziz Multimedia Entertainment and Bittoco 2024, all rights reserved.
 
-command=$1
+# /c/xampp/bitqobo/tools/compile-scss.sh true dev
+# 1st parameter: if true, this loads sass.bat instead of sass. this is for me (chaziz)
+# 2nd parameter: enables "--watch" in sass. i guess.
+
+command=$2
+executable=$1
 common_arguments="--style expanded --no-source-map --load-path ./"
 
 if [ "$command" = "dev" ]; then
@@ -19,13 +24,11 @@ fi
 # load sass.bat on windows because when you download a windows build of dart-sass,
 # the filename for it is "sass.bat".
 
-case "$(uname -s)" in
-    CYGWIN*|MINGW*|MSYS*)
-        sass_executable="sass.bat"
-        ;;
-    *)
-        sass_executable="sass"
-        ;;
-esac
+sass_executable="sass"
+
+if [ "$executable" = "true" ]; then
+    # for those on windows who installed dart-sass via a zip on github and not scoop/choco/whatever
+    sass_executable="sass.bat"
+fi
 
 $sass_executable $common_arguments scss/:public/img/
