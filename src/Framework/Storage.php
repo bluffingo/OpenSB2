e1f8cc27ef19a55d3325423491c22ecd7c540484
Hedy88 <diab.hedy@gmail.com>
2024-05-11 17:51:07 +0100

    add Qobo\Framework\Storage interface

---
+++ src/Framework/Storage.php
@@ -0,0 +1,13 @@
+<?php
+// Copyright Chaziz Multimedia Entertainment and Bittoco 2024, all rights reserved.
+
+namespace Qobo\Framework;
+
+interface Storage {
+    public function processVideo($new, $target_file);
+    public function processImage($new, $target_file);
+    public function processMusic($new, $target_file);
+    public function processFlash($new, $target_file);
+
+    public function uploadProfilePicture($temp_name, $new);
+}
